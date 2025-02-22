ec4d7f6a43d737f3ae5606ac9ef3ce66e8b34438
anatolyguari <beorekisan@gmail.com>
2025-02-23 01:10:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -115,5 +115,6 @@ def handle_clear_chat():
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    socketio.run(app, debug=True, host="0.0.0.0", port=port)
+    socketio.run(app, debug=True, host="0.0.0.0", port=port, allow_unsafe_werkzeug=True)
+
 
